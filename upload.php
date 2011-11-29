9cb09d8dd6ec9306f214650689e450a05c069190
unknown <naoto.nishio@cs237-09.csisad.ul.campus>
2011-11-29 10:31:41 +0000

    Add Bing

--- upload.php
+++ upload.php
@@ -227,8 +227,9 @@
 		}
 	
 		else{
-			header('Content-Type: text/html; charset=utf-8');		
-//p
+			header('Content-Type: text/html; charset=utf-8');	
+			
+?>
 			
 						<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 						<html xmlns="http://www.w3.org/1999/xhtml"><head>
@@ -264,7 +265,7 @@
 						</div>
 						</div>
 						</body></html>
-//php			
+<?php		
 			}
 		} //It worked.
       else
