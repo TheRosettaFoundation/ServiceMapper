44b6bcbbe94170110b4854dadefcfce68a06c8bb
Manuel Honegger <manuel_honegger@outlook.com>
2012-12-03 16:36:56 +0000

    ProviderHelper

--- ProviderHelper.php
+++ ProviderHelper.php
@@ -39,6 +39,50 @@ class ProviderHelper {
         }
         return $providers;
     }
+    
+    public static function enrichFile( &$file, array $array) {
+        foreach ($array as $xliff){
+            ProviderHelper::mergeFile($file, $xliff);
+        }
+    }
+    
+    public static function mergeFile(&$file, &$file2) { //private
+        //merge altrans from file2  into file per transunit 
+        
+        //$transUnits = $file->getElementsByTagName("trans-unit"); 
+        if($altTransUnits = $file2->getElementsByTagName("alt-trans")) {
+        
+        //foreach($transUnits as $transUnit ){
+            foreach($altTransUnits as $altTransUnit) {    
+                
+                $alt_trans = $file->createElement("alt-trans");
+                $alt_trans->appendChild($file->createTextNode($altTransUnits->item(0)->nodeValue));
+                //$alt_trans->appendChild($altTarget); 
+                
+            }
+            
+            if($data = $file->saveXML()) {
+                 return $data;
+            } else {
+                echo "Failed to dump XML tree to string";
+            }            
+        //}
+        } else {
+            echo "No alt-trans elements found";
+        }
+        
+    }    
 }
+$fileText = "/home/manuel/Desktop/ExampleDocs/lucia/Symposium3.xlf";
+$fileText2 = "/home/manuel/Desktop/ExampleDocs/lucia/alt-trans.xlf";
+$doc = new DOMDocument();
+$doc2 = new DOMDocument();
+if($doc->load($fileText)) {
+    if($doc2->load($fileText2)) {
+        echo ProviderHelper::mergeFile($doc, $doc2);        
+    }
+}
+
+    
 
 ?>
