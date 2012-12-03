d511d6cb29cf7d7888de156b3956d9cbeb961e33
Manuel <manuel_honegger@outlook.com>
2012-12-03 23:15:59 +0000

    Further updates to mergeFile function;

--- ProviderHelper.php
+++ ProviderHelper.php
@@ -1,5 +1,6 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
+mb_internal_encoding('UTF-8');
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -55,27 +56,65 @@ class ProviderHelper {
     public static function mergeFile(&$file, &$file2) { //private
         //merge altrans from file2  into file per transunit 
         
-        //$transUnits = $file->getElementsByTagName("trans-unit"); 
-        if($altTransUnits = $file2->getElementsByTagName("alt-trans")) {
+        $transUnits = $file->getElementsByTagName("trans-unit"); 
+        $altTransUnits = $file2->getElementsByTagName("alt-trans");   
         
-        //foreach($transUnits as $transUnit ){
-            foreach($altTransUnits as $altTransUnit) {    
-                
-                $alt_trans = $file->createElement("alt-trans");
-                $alt_trans->appendChild($file->createTextNode($altTransUnits->item(0)->nodeValue));
-                //$alt_trans->appendChild($altTarget); 
-                
-            }
+        for($i=0; $i < $altTransUnits->length; $i++) {
             
-            if($data = $file->saveXML()) {
-                 return $data;
-            } else {
-                echo "Failed to dump XML tree to string";
-            }            
-        //}
-        } else {
-            echo "No alt-trans elements found";
+            $transUnit = $transUnits->item($i);
+            $altTransUnit = $altTransUnits->item($i);
+                
+            if($altTransUnit->hasChildNodes()) {
+
+                $altTrans = $file->createElement("alt-trans");
+                $altTransSource = $file->createElement("source");
+                $altTransTarget = $file->createElement("target");
+
+                $children = $altTransUnit->childNodes;
+
+                foreach($children as $child) {
+                    if($child->nodeName == "source") {
+                        //$sourceAltTransValue = $child->nodeValue;
+                        $altTransSource->appendChild($file->createTextNode($child->nodeValue));
+                        //$x = $child->getAttribute("xml:lang");
+                        if($child->hasAttributeNS("xml","lang")) {
+                            //$attributeValue = $child->getAttributeNS("xml","lang");
+                            $altTransSource->setAttributeNS("xml","lang", $child->getAttributeNS("xml","lang"));
+                        }                            
+
+                    }elseif($child->nodeName == "target") {
+                        //$targetAltTransValue = $child->nodeValue;
+                        $altTransTarget->appendChild($file->createTextNode($child->nodeValue));
+
+                        if($child->hasAttributeNS("xml","lang")) {
+                            //$attributeValue = $child->getAttributeNS("xml","lang");
+                            $altTransTarget->setAttributeNS("xml","lang", $child->getAttributeNS("xml","lang"));
+                        }                            
+                    }
+                }
+
+                $altTrans->appendChild($altTransSource);
+                $altTrans->appendChild($altTransTarget);
+                $transUnit->appendChild($altTrans);
+
+                echo "has child nodes <br>";
+            }
         }
         
-    }    
+        if($data = $file->saveXML()) {
+            return $data;
+        } else {
+            echo "Failed to dump XML tree to string";
+        }            
+    }
 }
+
+$file = "uploads/test1.xlf";
+$file2 = "uploads/test2.xlf";
+$doc = new DOMDocument();
+$doc2 = new DOMDocument();
+if($doc->load($file)) {
+    if($doc2->load($file2)) {
+        echo "<br>xml tree dump: ".ProviderHelper::mergeFile($doc, $doc2);        
+    }
+} 
